4b9460601624ce15526dc33a2ae72005ab7bc672
Rogier Spieker <rogier@konfirm.eu>
2021-04-28 11:12:40 +0200

    bitwise operators

---
+++ source/Domain/Operator/Bitwise.ts
@@ -0,0 +1,73 @@
+import type { Evaluator } from '../Query/Compiler';
+
+type BitMask = number;
+type BitPosition = Array<number>;
+
+export type Operation = {
+	$bitsAllClear: Parameters<typeof $bitsAllClear>[0];
+	$bitsAllSet: Parameters<typeof $bitsAllSet>[0];
+	$bitsAnyClear: Parameters<typeof $bitsAnyClear>[0];
+	$bitsAnySet: Parameters<typeof $bitsAnySet>[0];
+};
+
+/**
+ * $bitsAllClear
+ * Matches numeric or binary values in which a set of bit positions all have a value of 0.
+  * @syntax  { <field>: { $bitsAllClear: <numeric bitmask> } }
+  *          { <field>: { $bitsAllClear: [ <position1>, <position2>, ...] } }
+ * @see     https://docs.mongodb.com/manual/reference/operator/query/bitsAllClear/
+ */
+export function $bitsAllClear(query: BitMask | BitPosition): Evaluator {
+	if (Array.isArray(query)) {
+		const shifted = query.map((shift) => 1 << shift);
+
+		return (input: unknown): boolean => shifted.every((bit) => (Number(input) & bit) === 0);
+	}
+
+	return (input: unknown): boolean => (Number(input) & Number(query)) === 0;
+}
+
+/**
+ * $bitsAllSet
+ * Matches numeric or binary values in which a set of bit positions all have a value of 1.
+ * @syntax  { <field>: { $bitsAllSet: <numeric bitmask> } }
+ *          { <field>: { $bitsAllSet: [ <position1>, <position2>, ...] } }
+ * @see     https://docs.mongodb.com/manual/reference/operator/query/bitsAllSet/
+ */
+export function $bitsAllSet(query: BitMask | BitPosition): Evaluator {
+	if (Array.isArray(query)) {
+		return (input: unknown): boolean => query.every((shift) => Number(input) & 1 << shift);
+	}
+
+	return (input: unknown): boolean => (Number(input) & Number(query)) === query;
+}
+
+/**
+ * $bitsAnyClear
+ * Matches numeric or binary values in which any bit from a set of bit positions has a value of 0.
+ * @syntax  { <field>: { $bitsAnyClear: <numeric bitmask> } }
+ *          { <field>: { $bitsAnyClear: [ <position1>, <position2>, ...] } }
+ * @see     https://docs.mongodb.com/manual/reference/operator/query/bitsAnyClear/
+ */
+export function $bitsAnyClear(query: BitMask | BitPosition): Evaluator {
+	if (Array.isArray(query)) {
+		return (input: unknown): boolean => query.some((shift) => (Number(input) & 1 << shift) !== 1 << shift);
+	}
+
+	return (input: unknown): boolean => (Number(input) & Number(query)) !== query;
+}
+
+/**
+ * $bitsAnySet
+ * Matches numeric or binary values in which any bit from a set of bit positions has a value of 1.
+ * @syntax  { <field>: { $bitsAnySet: <numeric bitmask> } }
+ *          { <field>: { $bitsAnySet: [ <position1>, <position2>, ...] } }
+ * @see     https://docs.mongodb.com/manual/reference/operator/query/bitsAnySet/
+ */
+export function $bitsAnySet(query: BitMask | BitPosition): Evaluator {
+	if (Array.isArray(query)) {
+		return (input: unknown): boolean => query.some((shift) => (Number(input) & 1 << shift));
+	}
+
+	return (input: unknown): boolean => (Number(input) & Number(query)) !== 0;
+}
---
+++ test/Domain/Operator/Bitwise.ts
@@ -0,0 +1,169 @@
+import * as test from 'tape';
+import each from 'template-literal-each';
+import * as Bitwise from '../../../source/Domain/Operator/Bitwise';
+
+type TestRecord = { query: number | Array<number>, input: number, matches: string };
+
+test('Domain/Operator/Bitwise - exports', (t) => {
+	const expected = ['$bitsAllClear', '$bitsAllSet', '$bitsAnyClear', '$bitsAnySet'];
+	const actual = Object.keys(Bitwise);
+
+	t.equal(actual.length, expected.length, `contains ${expected.length} keys`);
+	expected.forEach((key) => {
+		t.equal(typeof Bitwise[<keyof typeof Bitwise>key], 'function', `contains function ${key}`);
+	});
+
+	t.end();
+});
+
+test('Domain/Operator/Bitwise - $bitsAllClear', (t) => {
+	const { $bitsAllClear } = Bitwise;
+
+	each`
+		query     | input | matches
+		----------|-------|---------
+		${1}      | ${0}  | yes
+		${1}      | ${1}  | no
+		${1}      | ${2}  | yes
+		${2}      | ${2}  | no
+		${3}      | ${0}  | yes
+		${3}      | ${1}  | no
+		${3}      | ${2}  | no
+		${3}      | ${3}  | no
+		${3}      | ${4}  | yes
+		${[0, 2]} | ${0}  | yes
+		${[0, 2]} | ${1}  | no
+		${[0, 2]} | ${2}  | yes
+		${[0, 2]} | ${3}  | no
+		${[0, 2]} | ${4}  | no
+		${[0, 2]} | ${5}  | no
+		${[0, 2]} | ${6}  | no
+		${[0, 2]} | ${7}  | no
+		${[0, 2]} | ${8}  | yes
+		${[200]}  | ${-5} | no
+		${[200]}  | ${5}  | yes
+	`((record) => {
+		const { query, input, matches } = <TestRecord>record;
+		const compiled = $bitsAllClear(query);
+		const isMatch = matches === 'yes';
+		const message = isMatch ? 'matches' : 'does not match'
+
+		t.equal(compiled(input), matches === 'yes', `${input} ${message} ${JSON.stringify(query)}`);
+	});
+
+	t.end();
+});
+
+test('Domain/Operator/Bitwise - $bitsAllSet', (t) => {
+	const { $bitsAllSet } = Bitwise;
+
+	each`
+		query     | input | matches
+		----------|-------|---------
+		${1}      | ${0}  | no
+		${1}      | ${1}  | yes
+		${1}      | ${2}  | no
+		${2}      | ${2}  | yes
+		${3}      | ${0}  | no
+		${3}      | ${1}  | no
+		${3}      | ${2}  | no
+		${3}      | ${3}  | yes
+		${3}      | ${4}  | no
+		${[0, 2]} | ${0}  | no
+		${[0, 2]} | ${1}  | no
+		${[0, 2]} | ${2}  | no
+		${[0, 2]} | ${3}  | no
+		${[0, 2]} | ${4}  | no
+		${[0, 2]} | ${5}  | yes
+		${[0, 2]} | ${6}  | no
+		${[0, 2]} | ${7}  | yes
+		${[0, 2]} | ${8}  | no
+		${[200]}  | ${-5} | yes
+		${[200]}  | ${5}  | no
+	`((record) => {
+		const { query, input, matches } = <TestRecord>record;
+		const compiled = $bitsAllSet(query);
+		const isMatch = matches === 'yes';
+		const message = isMatch ? 'matches' : 'does not match'
+
+		t.equal(compiled(input), matches === 'yes', `${input} ${message} ${JSON.stringify(query)}`);
+	});
+
+	t.end();
+});
+
+test('Domain/Operator/Bitwise - $bitsAnyClear', (t) => {
+	const { $bitsAnyClear } = Bitwise;
+
+	each`
+		query     | input | matches
+		----------|-------|---------
+		${1}      | ${0}  | yes
+		${1}      | ${1}  | no
+		${1}      | ${2}  | yes
+		${2}      | ${2}  | no
+		${3}      | ${0}  | yes
+		${3}      | ${1}  | yes
+		${3}      | ${2}  | yes
+		${3}      | ${3}  | no
+		${3}      | ${4}  | yes
+		${[0, 2]} | ${0}  | yes
+		${[0, 2]} | ${1}  | yes
+		${[0, 2]} | ${2}  | yes
+		${[0, 2]} | ${3}  | yes
+		${[0, 2]} | ${4}  | yes
+		${[0, 2]} | ${5}  | no
+		${[0, 2]} | ${6}  | yes
+		${[0, 2]} | ${7}  | no
+		${[0, 2]} | ${8}  | yes
+		${[200]}  | ${-5} | no
+		${[200]}  | ${5}  | yes
+	`((record) => {
+		const { query, input, matches } = <TestRecord>record;
+		const compiled = $bitsAnyClear(query);
+		const isMatch = matches === 'yes';
+		const message = isMatch ? 'matches' : 'does not match'
+
+		t.equal(compiled(input), matches === 'yes', `${input} ${message} ${JSON.stringify(query)}`);
+	});
+
+	t.end();
+});
+
+test('Domain/Operator/Bitwise - $bitsAnySet', (t) => {
+	const { $bitsAnySet } = Bitwise;
+
+	each`
+		query     | input | matches
+		----------|-------|---------
+		${1}      | ${0}  | no
+		${1}      | ${1}  | yes
+		${1}      | ${2}  | no
+		${2}      | ${2}  | yes
+		${3}      | ${0}  | no
+		${3}      | ${1}  | yes
+		${3}      | ${2}  | yes
+		${3}      | ${3}  | yes
+		${3}      | ${4}  | no
+		${[0, 2]} | ${0}  | no
+		${[0, 2]} | ${1}  | yes
+		${[0, 2]} | ${2}  | no
+		${[0, 2]} | ${3}  | yes
+		${[0, 2]} | ${4}  | yes
+		${[0, 2]} | ${5}  | yes
+		${[0, 2]} | ${6}  | yes
+		${[0, 2]} | ${7}  | yes
+		${[0, 2]} | ${8}  | no
+		${[200]}  | ${-5} | yes
+		${[200]}  | ${5}  | no
+	`((record) => {
+		const { query, input, matches } = <TestRecord>record;
+		const compiled = $bitsAnySet(query);
+		const isMatch = matches === 'yes';
+		const message = isMatch ? 'matches' : 'does not match'
+
+		t.equal(compiled(input), matches === 'yes', `${input} ${message} ${JSON.stringify(query)}`);
+	});
+
+	t.end();
+});
