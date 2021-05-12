4c3da976603c59506dc59761d7a6e612b8fb6f49
Rogier Spieker <rogier@konfirm.eu>
2021-05-12 13:59:20 +0200

    Ensure 100% coverage on logical query operators

---
+++ test/Samples/Logical.ts
@@ -0,0 +1,139 @@
+import * as test from 'tape';
+import each from 'template-literal-each';
+import { compile } from '../../source/main';
+
+test('Samples/Logical - $and', (t) => {
+	const query = {
+		$and: [
+			{ name: { $exists: true } },
+			{ age: { $gte: 21 } },
+		]
+	};
+	const and = compile(query);
+
+	each`
+		name     | age   | matches
+		---------|-------|---------
+		         |       | no
+		         | ${20} | no
+		         | ${21} | no
+		Foo      |       | no
+		Foo      | ${20} | no
+		Foo      | ${21} | yes
+		${true}  | ${21} | yes
+		${false} | ${21} | yes
+		${null}  | ${21} | no
+		${1}     | ${21} | yes
+	`((record) => {
+		const { name, age, matches } = record as { name: any, age?: number, matches: string };
+		const isMatch = matches === 'yes';
+		const message = isMatch ? 'matches' : 'does not match';
+		const input = { name, age };
+
+		t.equal(and(input), isMatch, `${JSON.stringify(input)} ${message} ${JSON.stringify(query)}`)
+	});
+
+	t.end();
+});
+
+test('Samples/Logical - $not', (t) => {
+	const query = {
+		$not: {
+			name: { $exists: true },
+			age: { $gte: 21 },
+		}
+	};
+	const not = compile(query);
+
+	each`
+		name     | age   | matches
+		---------|-------|---------
+		         |       | yes
+		         | ${20} | yes
+		         | ${21} | yes
+		Foo      |       | yes
+		Foo      | ${20} | yes
+		Foo      | ${21} | no
+		${true}  | ${21} | no
+		${false} | ${21} | no
+		${null}  | ${21} | yes
+		${1}     | ${21} | no
+	`((record) => {
+		const { name, age, matches } = record as { name: any, age?: number, matches: string };
+		const isMatch = matches === 'yes';
+		const message = isMatch ? 'matches' : 'does not match';
+		const input = { name, age };
+
+		t.equal(not(input), isMatch, `${JSON.stringify(input)} ${message} ${JSON.stringify(query)}`)
+	});
+
+	t.end();
+});
+
+test('Samples/Logical - $nor', (t) => {
+	const query = {
+		$nor: [
+			{ name: { $exists: true } },
+			{ age: { $gte: 21 } },
+		]
+	};
+	const nor = compile(query);
+
+	each`
+		name     | age   | matches
+		---------|-------|---------
+		         |       | yes
+		         | ${20} | yes
+		         | ${21} | no
+		Foo      |       | no
+		Foo      | ${20} | no
+		Foo      | ${21} | no
+		${true}  | ${21} | no
+		${false} | ${21} | no
+		${null}  | ${21} | no
+		${1}     | ${21} | no
+	`((record) => {
+		const { name, age, matches } = record as { name: any, age?: number, matches: string };
+		const isMatch = matches === 'yes';
+		const message = isMatch ? 'matches' : 'does not match';
+		const input = { name, age };
+
+		t.equal(nor(input), isMatch, `${JSON.stringify(input)} ${message} ${JSON.stringify(query)}`)
+	});
+
+	t.end();
+});
+
+test('Samples/Logical - $or', (t) => {
+	const query = {
+		$or: [
+			{ name: { $exists: true } },
+			{ age: { $gte: 21 } },
+		]
+	};
+	const or = compile(query);
+
+	each`
+		name     | age   | matches
+		---------|-------|---------
+		         |       | no
+		         | ${20} | no
+		         | ${21} | yes
+		Foo      |       | yes
+		Foo      | ${20} | yes
+		Foo      | ${21} | yes
+		${true}  | ${21} | yes
+		${false} | ${21} | yes
+		${null}  | ${21} | yes
+		${1}     | ${21} | yes
+	`((record) => {
+		const { name, age, matches } = record as { name: any, age?: number, matches: string };
+		const isMatch = matches === 'yes';
+		const message = isMatch ? 'matches' : 'does not match';
+		const input = { name, age };
+
+		t.equal(or(input), isMatch, `${JSON.stringify(input)} ${message} ${JSON.stringify(query)}`)
+	});
+
+	t.end();
+});
